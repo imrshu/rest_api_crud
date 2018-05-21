9b2b7d82d339e0180767fe180ec6aa8d05741720
Nitin Raturi <raturi002@gmail.com>
2018-05-21 16:17:57 +0530

    First View

--- rest_api_crud/urls.py
+++ rest_api_crud/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from data import views as dataView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',dataView.home)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
